49083ea194abe68ea31d0fc7d58bb7825ad93e59
Dominik Maier <domenukk@gmail.com>
2019-12-18 12:22:48 +0100

    disabled debug

--- config.mk
+++ config.mk
@@ -8,7 +8,7 @@
 # Compile with debug info when you want to debug code.
 # Change this to 'no' for release edition.
 
-UNICORN_DEBUG ?= yes
+UNICORN_DEBUG ?= no
 
 ################################################################################
 # Specify which archs you want to compile in. By default, we build all archs.
