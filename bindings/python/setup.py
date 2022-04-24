09ad7d4784e50ec4ddf590a2c29764e2a7f37442
lazymio <mio@lazym.io>
2022-04-24 20:11:25 +0200

    Bump unicorn to rc7

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -145,7 +145,7 @@ setuptools.setup(
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        "unicorn>=2.0.0rc6"
+        "unicorn>=2.0.0rc7"
     ],
     is_pure=False,
     package_data={
