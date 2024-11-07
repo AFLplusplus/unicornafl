2abdcd3c796042eaf62ed03e57bcd4199d12b935
mio <mio@lazym.io>
2024-11-07 21:59:33 +0800

    Force 2.0.1.post1 until we have 2.1.2

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -145,7 +145,7 @@ setuptools.setup(
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        "unicorn>=2.0.1"
+        "unicorn==2.0.1.post1"
     ],
     is_pure=False,
     package_data={
