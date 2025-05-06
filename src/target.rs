99c011bdcbc9c86e61ea51e9041abf028c1f4efd
Evian-Zhang <evianzhang1999@163.com>
2025-05-06 20:05:54 +0800

    Migrate to newest libafl

--- src/target.rs
+++ src/target.rs
@@ -1,26 +1,11 @@
 use std::path::PathBuf;
 
-use libafl::{
-    corpus::{Corpus, InMemoryCorpus, Testcase},
-    events::SimpleEventManager,
-    feedbacks::{BoolValueFeedback, CrashFeedback},
-    monitors::SimpleMonitor,
-    schedulers::QueueScheduler,
-    state::StdState,
-    Fuzzer, StdFuzzer,
-};
-use libafl_bolts::{
-    ownedref::OwnedSlice,
-    rands::StdRand,
-    tuples::{tuple_list, Handle},
-};
-use libafl_targets::{EDGES_MAP_SIZE, SHM_FUZZING};
-use log::{debug, trace, warn};
+use libafl_targets::{__afl_map_size, EDGES_MAP_PTR, SHM_FUZZING};
+use log::{debug, error, trace, warn};
 use unicorn_engine::{uc_error, Arch, RegisterARM, Unicorn};
 
 use crate::{
-    executor::{UnicornAflExecutor, UnicornFuzzData, UnsafeSliceInput},
-    harness::LegacyHarnessStage,
+    executor::{UnicornAflExecutor, UnicornAflExecutorHook, UnicornFuzzData},
     uc_afl_ret,
 };
 
@@ -33,7 +18,7 @@ pub fn dummy_uc_fuzz_callback<'a, D: 'a>(
     let mut pc = uc.pc_read()?;
     if arch == Arch::ARM {
         let cpsr = uc.reg_read(RegisterARM::CPSR)?;
-        if cpsr & 0x20 == 1 {
+        if cpsr & 0x20 != 0 {
             pc |= 1;
         }
     }
@@ -51,15 +36,14 @@ pub fn dummy_uc_validate_crash_callback<'a, D: 'a>(
     unicorn_result.is_err()
 }
 
-/// Internal entrypoint for fuzzing
+/// Internal entrypoint for fuzzing.
+///
+/// This is only expected to be runned in forkserver mode.
 pub fn child_fuzz<'a, D: 'a>(
     uc: Unicorn<'a, UnicornFuzzData<D>>,
     input_file: Option<PathBuf>,
     iters: u32,
-    place_input_cb: impl FnMut(&mut Unicorn<'a, UnicornFuzzData<D>>, &[u8], u64) -> bool + 'a,
-    validate_crash_cb: impl FnMut(&mut Unicorn<'a, UnicornFuzzData<D>>, Result<(), uc_error>, &[u8], u64) -> bool
-        + 'a,
-    fuzz_callback: impl FnMut(&mut Unicorn<'a, UnicornFuzzData<D>>) -> Result<(), uc_error> + 'a,
+    callbacks: impl UnicornAflExecutorHook<'a, D>,
     exits: Vec<u64>,
     always_validate: bool,
     run_once_if_no_afl_present: bool,
@@ -68,67 +52,54 @@ pub fn child_fuzz<'a, D: 'a>(
     #[cfg(feature = "env_logger")]
     env_logger::init();
 
-    let has_afl = libafl_targets::map_input_shared_memory() && libafl_targets::map_shared_memory();
+    let has_afl = libafl_targets::map_input_shared_memory().is_ok()
+        && libafl_targets::map_shared_memory().is_ok();
 
-    trace!("AFL detected: {}", has_afl);
-    if !input_file.is_none() && has_afl {
+    trace!("AFL detected: {has_afl}");
+    if input_file.is_some() && has_afl {
         warn!("Shared memory fuzzing is enabled and the input file is ignored!");
     }
     if input_file.is_none() && !has_afl {
         warn!("No input file is provided. We will run harness with zero inputs.");
     }
+    let mut local_map;
+    if !has_afl && run_once_if_no_afl_present {
+        let map_size = uc.get_data().map_size();
+        // This local variable will never be freed until current function is end, which
+        // is after the forkserver loop.
+        local_map = vec![0u8; map_size as usize];
+        unsafe { EDGES_MAP_PTR = local_map.as_mut_ptr() }
+        // If no afl, input will be read from input_file in forkserver_run_harness,
+        // thus no need to setup INPUT_PTR and INPUT_LENGTH_PTR
+    }
     if has_afl || run_once_if_no_afl_present {
         let map_size = uc.get_data().map_size();
         unsafe {
-            EDGES_MAP_SIZE = map_size as usize;
+            __afl_map_size = map_size as usize;
             SHM_FUZZING = 1;
         }
-        libafl_targets::start_forkserver();
-        // Only child returns here
-        let map_size = unsafe { EDGES_MAP_SIZE };
-        debug!("Map size is: {}", map_size);
-        let mut executor = UnicornAflExecutor::new(
-            uc,
-            place_input_cb,
-            validate_crash_cb,
-            fuzz_callback,
-            always_validate,
-            exits,
-        )?;
+        debug!("Map size is: {map_size}");
+        let executor = UnicornAflExecutor::new(uc, (), callbacks, always_validate, exits)?;
+        let mut forkserver_parent = crate::forkserver::UnicornAflForkserverParent::new(executor);
+        libafl_targets::start_forkserver(&mut forkserver_parent)?;
+        let mut executor = forkserver_parent.executor;
 
-        let mut fb = BoolValueFeedback::new(&Handle::new("dumb_ob".into()));
-        let mut sol = CrashFeedback::new();
-        let mut corpus = InMemoryCorpus::new();
-        corpus.add(Testcase::new(UnsafeSliceInput {
-            input: OwnedSlice::from(Vec::<u8>::new()),
-        }))?;
-        let mut state = StdState::new(
-            StdRand::new(),
-            corpus,
-            InMemoryCorpus::new(),
-            &mut fb,
-            &mut sol,
-        )?;
-
-        let mut mgr = SimpleEventManager::new(SimpleMonitor::new(|s| {
-            debug!("{}", s);
-        }));
-        let sched = QueueScheduler::new();
         let iters = if !has_afl && run_once_if_no_afl_present {
             1
         } else {
             iters
         };
         let input_file = if has_afl { None } else { input_file };
-        let stage = LegacyHarnessStage::new(iters as usize, map_size, input_file);
-        let mut stages = tuple_list!(stage);
-        let mut fuzzer = StdFuzzer::new(sched, fb, sol);
-
-        if let Err(e) = fuzzer.fuzz_one(&mut stages, &mut executor, &mut state, &mut mgr) {
-            warn!("Fuzzing fails with error from libafl: {}", e);
+        if let Err(e) =
+            crate::harness::forkserver_run_harness(&mut executor, input_file, iters as usize)
+        {
+            // The error cannot be propagated since we are in child process now.
+            // So just log.
+            error!("Fuzzing fails with error from libafl: {e}");
         }
     } else {
         // Run with libafl directly
+        unimplemented!("You should use UnicornAflExecutor directly to compose your fuzzer")
     }
     Ok(())
 }
